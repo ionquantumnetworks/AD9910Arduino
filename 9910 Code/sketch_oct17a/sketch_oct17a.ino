9a64feedcb66c49a60c8f6c41df7b4d5f0a8fae2
James Siverns <jsiverns@umd.edu>
2019-10-29 14:46:39 -0400

    code works for single tone output - check freq word for accuracy

--- 9910 Code/sketch_oct17a/sketch_oct17a.ino
+++ 9910 Code/sketch_oct17a/sketch_oct17a.ino
@@ -2,53 +2,60 @@
 
 void setup() {
   // initialize SPI:
+  SPISettings settingsA(1000000, MSBFIRST, SPI_MODE0);
   SPI.begin();
   digitalWrite(SS,HIGH); //the AD9910 reads data when this is low. So set to high to begin
   SPI.setBitOrder(MSBFIRST); //set the data mode to most significant bit. The AD9910 has this as default
   pinMode(2,OUTPUT); //set pin 2 to be an output pin
+  pinMode(3,OUTPUT); //set pin 3 to be an output pin
+  pinMode(4,OUTPUT); //set pin 4 to be an output pin
   digitalWrite(2, LOW); //Set the IOupdate line to LOW as a HIGH value sends data to DDS from register
-//  int data = 110;
-//  while(data < 100 ){
-//    digitalWrite(SS, LOW);
-//    SPI.transfer(data);
-//    SPI.transfer(data);
-//    digitalWrite(SS,HIGH);
-//    data++;
-//    delay(500);}
-//CFR1 Bytes
-  byte CFR1InstructionByte = B0110000;
-  byte CFR1Byte1 = 0x00;
-  byte CFR1Byte2 = 0x00; 
-  byte CFR1Byte3 = 0x00; 
-  byte CFR1Byte4 = 0x00; 
+   //CFR1 Bytes
+  byte CFR1InstructionByte = B00000000;
+  byte CFR1Byte1 = B00000000;
+  byte CFR1Byte2 = B00000000; 
+  byte CFR1Byte3 = B00000000; 
+  byte CFR1Byte4 = B00000010; //0x00
 //CFR2 Bytes
-  byte CFR2InstructionByte = B0110001;
-  byte CFR2Byte1 = 0x01;
-  byte CFR2Byte2 = 0x40; 
-  byte CFR2Byte3 = 0x08; 
-  byte CFR2Byte4 = 0x20; 
+  byte CFR2InstructionByte = B00000001;
+  byte CFR2Byte1 = B00000000; //0x01
+  byte CFR2Byte2 = B01000000; 
+  byte CFR2Byte3 = B00001000; 
+  byte CFR2Byte4 = B00100000; 
 //CFR3 Bytes
-  byte CFR3InstructionByte = B0110010;
-  byte CFR3Byte1 = 0x1F;
-  byte CFR3Byte2 = 0x3F; 
-  byte CFR3Byte3 = 0x40; 
-  byte CFR3Byte4 = 0x00; 
+  byte CFR3InstructionByte = B00000010;
+  byte CFR3Byte1 = B00011111;
+  byte CFR3Byte2 = B00111111; 
+  byte CFR3Byte3 = B11000000; //0x40
+  byte CFR3Byte4 = B00000000; 
 //Aux DAC control
-  byte ADCInstructionByte = B0110011;;
-  byte ADCByte1 = 0x00;
-  byte ADCByte2 = 0x00; 
-  byte ADCByte3 = 0x7F; 
-  byte ADCByte4 = 0x7F; 
+  byte ADCInstructionByte = B00000011;
+  byte ADCByte1 = B00000000;
+  byte ADCByte2 = B00000000; 
+  byte ADCByte3 = B01111111; 
+  byte ADCByte4 = B01111111; 
+  //I/O update Rate
+  byte IOInstructionByte = B00000100;
+  byte IOByte1 = B11111111;
+  byte IOByte2 = B11111111; 
+  byte IOByte3 = B11111111; 
+  byte IOByte4 = B11111111; 
 //Single Tone Data
-  byte InstructionByte = B0111110;; //Byte to tell DDS what the following data is
-  byte DataByte1 = 0x20; //Amplitude scale factor
-  byte DataByte2 = 0x00; //Amplitude scale factor
-  byte DataByte3 = 0x00; //Phase Offset
-  byte DataByte4 = 0x00; //Phase Offset
-  byte DataByte5 = 0x14; //Frequency
-  byte DataByte6 = 0x7A; //Frequency
-  byte DataByte7 = 0xE1; //Frequency
-  byte DataByte8 = 0x48; //Frequency
+  byte InstructionByte = B00001110;; //Byte to tell DDS what the following data is
+  byte DataByte1 = B00001000; //Amplitude scale factor
+  byte DataByte2 = B10110101; //Amplitude scale factor
+  byte DataByte3 = B00000000; //Phase Offset
+  byte DataByte4 = B00000000; //Phase Offset
+  byte DataByte5 = B00000000; //Frequency
+  byte DataByte6 = B01000001; //Frequency
+  byte DataByte7 = B10001001; //Frequency
+  byte DataByte8 = B00110111; //Frequency
+  digitalWrite(4, HIGH);//Reset the board
+  delay(500);
+  digitalWrite(4, LOW);//Reset the board
+  digitalWrite(3, HIGH);//Reset the I/O communication
+  delay(500);
+  digitalWrite(3, LOW);//Reset the I/O communication
   digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
   delay(500);
   SPI.transfer(CFR1InstructionByte);
@@ -64,6 +71,7 @@ void setup() {
   digitalWrite(2, LOW);//Send the above data to DDS from register
   delay(500);
   digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
+  delay(500);
   SPI.transfer(CFR2InstructionByte);
   SPI.transfer(CFR2Byte1);
   SPI.transfer(CFR2Byte2);
@@ -77,6 +85,7 @@ void setup() {
   digitalWrite(2, LOW);//Send the above data to DDS from register
   delay(500);
   digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
+  delay(500);
   SPI.transfer(CFR3InstructionByte);
   SPI.transfer(CFR3Byte1);
   SPI.transfer(CFR3Byte2);
@@ -90,6 +99,7 @@ void setup() {
   digitalWrite(2, LOW);//Send the above data to DDS from register
   delay(500);
   digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
+  delay(500);
   SPI.transfer(ADCInstructionByte);
   SPI.transfer(ADCByte1);
   SPI.transfer(ADCByte2);
@@ -103,11 +113,12 @@ void setup() {
   digitalWrite(2, LOW);//Send the above data to DDS from register
   delay(500);
   digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
-  SPI.transfer(0x04);
-  SPI.transfer(0xFF);
-  SPI.transfer(0xFF);
-  SPI.transfer(0xFF);
-  SPI.transfer(0xFF);
+  delay(500);
+  SPI.transfer(IOInstructionByte);
+  SPI.transfer(IOByte1);
+  SPI.transfer(IOByte2);
+  SPI.transfer(IOByte3);
+  SPI.transfer(IOByte4);
   delay(500);
   digitalWrite(SS,HIGH);//set the write line to HIGH so that the DDS stops reading
   delay(500);
@@ -116,6 +127,7 @@ void setup() {
   digitalWrite(2, LOW);//Send the above data to DDS from register
   delay(500);
   digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
+  delay(500);
   SPI.transfer(InstructionByte);
   SPI.transfer(DataByte1);
   SPI.transfer(DataByte2);
@@ -131,8 +143,10 @@ void setup() {
   digitalWrite(2, HIGH);//Send the above data to DDS from register
   delay(500);
   digitalWrite(2, LOW);//Send the above data to DDS from register
+
 }
 
-void loop() {
 
+void loop() {
+ 
 }
