457c9462c0c14d416b4057481aad205c28ff8e05
James Siverns <jsiverns@umd.edu>
2019-10-31 18:35:01 -0400

    freq scan now works

--- AD9910_Freq_Sweep/AD9910_Freq_Sweep.ino
+++ AD9910_Freq_Sweep/AD9910_Freq_Sweep.ino
@@ -11,13 +11,11 @@ void setup() {
   int IOupdate = 2;
   int IOreset = 3;
   int DRCTL = 5;
-  int DROVER = 6;
-  int DRHOLD = 7;
+  int DRHOLD = 6;
   pinMode(IOupdate,OUTPUT); //set pin 2 to be an output pin
   pinMode(IOreset,OUTPUT); //set pin 3 to be an output pin
   pinMode(MasterReset,OUTPUT); //set pin 4 to be an output pin
   pinMode(DRCTL, OUTPUT);
-  pinMode(DROVER, OUTPUT);
   pinMode(DRHOLD, OUTPUT);
   digitalWrite(IOupdate, LOW); //Set the IOupdate line to LOW as a HIGH value sends data to DDS from register
   digitalWrite(IOreset, LOW); //Set the IOreset line to LOW as a HIGH value resets IO communication
@@ -25,13 +23,13 @@ void setup() {
 //CFR1 Bytes
   byte CFR1InstructionByte = 0x00; //Instruction Byte to tell DDS what the following data is
   byte CFR1Byte1 = 0x00;
-  byte CFR1Byte2 = 0x00; //0x40 to enable inverse sinc filter
+  byte CFR1Byte2 = 0x40; //0x40 to enable inverse sinc filter
   byte CFR1Byte3 = 0x00; 
   byte CFR1Byte4 = 0x02; //0x00
 //CFR2 Bytes
   byte CFR2InstructionByte = 0x01; //Instruction Byte to tell DDS what the following data is
-  byte CFR2Byte1 = 0x01; //0x01 to enable single tone amplitude data
-  byte CFR2Byte2 = 0x00; //0x40 to enable SYNC_CLK output
+  byte CFR2Byte1 = 0x00;
+  byte CFR2Byte2 = B00001100;//0x48
   byte CFR2Byte3 = 0x08; 
   byte CFR2Byte4 = 0x20; 
 //CFR3 Bytes
@@ -40,16 +38,32 @@ void setup() {
   byte CFR3Byte2 = 0x3F; 
   byte CFR3Byte3 = 0x40; //0x40
   byte CFR3Byte4 = 0x00; 
-//Single Tone Data
-  byte InstructionByte = 0x0E;; //Instruction Byte to tell DDS what the following data is
-  byte DataByte1 = 0x3F; //Amplitude scale factor
-  byte DataByte2 = 0xFF; //Amplitude scale factor
-  byte DataByte3 = 0x00; //Phase Offset
-  byte DataByte4 = 0x00; //Phase Offset
-  byte DataByte5 = 0x14; //Frequency
-  byte DataByte6 = 0x7A; //Frequency
-  byte DataByte7 = 0xE1; //Frequency
-  byte DataByte8 = 0x48; //Frequency
+//Digital Ramp Limit
+  byte DRLInstructionByte = 0x0B;
+  byte DRLByte1 = 0x33; //Upper limit Frequency
+  byte DRLByte2 = 0x33; //Upper limit Frequency
+  byte DRLByte3 = 0x33; //Upper limit Frequency
+  byte DRLByte4 = 0x33; //Upper limit Frequency
+  byte DRLByte5 = 0x05; //Lower limit Frequency
+  byte DRLByte6 = 0x1E; //Lower limit Frequency
+  byte DRLByte7 = 0xB8; //Lower limit Frequency
+  byte DRLByte8 = 0x52; //Lower limit Frequency
+//Digital Ramp Step
+  byte DRSInstructionByte = 0x0C;
+  byte DRSByte1 = 0x00; //Decrement Step
+  byte DRSByte2 = 0x0D; //Decrement Step
+  byte DRSByte3 = 0x1B; //Decrement Step
+  byte DRSByte4 = 0x71; //Decrement Step
+  byte DRSByte5 = 0x00; //Increment Step
+  byte DRSByte6 = 0x0D; //Increment Step
+  byte DRSByte7 = 0x1B; //Increment Step
+  byte DRSByte8 = 0x71; //Increment Step
+//Digital Ramp Rate
+  byte DRRInstructionByte = 0x0D;
+  byte DRRByte1 = 0xF4; //-ve slope rate
+  byte DRRByte2 = 0x24; //-ve slope rate
+  byte DRRByte3 = 0xF4; //+ve slope rate
+  byte DRRByte4 = 0x24; //+ve slope rate
 //RESETS
   digitalWrite(MasterReset, HIGH);//Reset the board
   delay(5);
@@ -105,21 +119,59 @@ void setup() {
   digitalWrite(IOupdate, HIGH);//Send the above data to DDS from register
   delay(5);
   digitalWrite(IOupdate, LOW);//Send the above data to DDS from register
-//Single Tone data transfer
+//Digital Ramp Limit data transfer
   digitalWrite(IOreset, HIGH);//Reset the I/O communication
   delay(5);
   digitalWrite(IOreset, LOW);//Reset the I/O communication
   digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
   delay(5);
-  SPI.transfer(InstructionByte);
-  SPI.transfer(DataByte1);
-  SPI.transfer(DataByte2);
-  SPI.transfer(DataByte3);
-  SPI.transfer(DataByte4);
-  SPI.transfer(DataByte5);
-  SPI.transfer(DataByte6);
-  SPI.transfer(DataByte7);
-  SPI.transfer(DataByte8);
+  SPI.transfer(DRLInstructionByte);
+  SPI.transfer(DRLByte1);
+  SPI.transfer(DRLByte2);
+  SPI.transfer(DRLByte3);
+  SPI.transfer(DRLByte4);
+  SPI.transfer(DRLByte5);
+  SPI.transfer(DRLByte6);
+  SPI.transfer(DRLByte7);
+  SPI.transfer(DRLByte8);
+  delay(5);
+  digitalWrite(SS,HIGH);//set the write line to HIGH so that the DDS stops reading
+  delay(5);
+  digitalWrite(IOupdate, HIGH);//Send the above data to DDS from register
+  delay(5);
+  digitalWrite(IOupdate, LOW);//Send the above data to DDS from register
+//Digital Ramp Step data transfer
+  digitalWrite(IOreset, HIGH);//Reset the I/O communication
+  delay(5);
+  digitalWrite(IOreset, LOW);//Reset the I/O communication
+  digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
+  delay(5);
+  SPI.transfer(DRSInstructionByte);
+  SPI.transfer(DRSByte1);
+  SPI.transfer(DRSByte2);
+  SPI.transfer(DRSByte3);
+  SPI.transfer(DRSByte4);
+  SPI.transfer(DRSByte5);
+  SPI.transfer(DRSByte6);
+  SPI.transfer(DRSByte7);
+  SPI.transfer(DRSByte8);
+  delay(5);
+  digitalWrite(SS,HIGH);//set the write line to HIGH so that the DDS stops reading
+  delay(5);
+  digitalWrite(IOupdate, HIGH);//Send the above data to DDS from register
+  delay(5);
+  digitalWrite(IOupdate, LOW);//Send the above data to DDS from register
+//Digital Ramp Rate data transfer
+  digitalWrite(IOreset, HIGH);//Reset the I/O communication
+  delay(5);
+  digitalWrite(IOreset, LOW);//Reset the I/O communication
+  digitalWrite(SS,LOW);//set the write line to LOW so that the DDS reads the following data
+  delay(5);
+  SPI.transfer(DRRInstructionByte);
+  SPI.transfer(DRRByte1);
+  SPI.transfer(DRRByte2);
+  SPI.transfer(DRRByte3);
+  SPI.transfer(DRRByte4);
   delay(5);
   digitalWrite(SS,HIGH);//set the write line to HIGH so that the DDS stops reading
   delay(5);
@@ -133,8 +185,17 @@ void loop() {
   int MasterReset = 4;
   int IOupdate = 2;
   int IOreset = 3;
+  int DRCTL = 5;
+  int DRHOLD = 6;
+  int i = 0;
   digitalWrite(IOupdate, LOW); //Set the IOupdate line to LOW as a HIGH value sends data to DDS from register
   digitalWrite(IOreset, LOW); //Set the IOReset line to LOW as a HIGH value resets IO communication
   digitalWrite(MasterReset, LOW); //Set the Master Reset line to LOW as a HIGH value resets chip to default values
   digitalWrite(SS,HIGH);//set the write line to HIGH so that the DDS stops reading
+  while(i < 100){
+    digitalWrite(DRCTL,HIGH);
+    //delay(1);
+    digitalWrite(DRCTL,LOW);
+    delay(5000);
+  }
 }
